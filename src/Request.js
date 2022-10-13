cbcfb42a5c4c9ef985fcc20a696d2086a8e4d345
Lucas Cabral <lucascabral6008@gmail.com>
2022-10-13 12:08:09 -0300

    api request and styling backdrop

---
+++ src/Request.js
@@ -0,0 +1,11 @@
+const key = "76470263ef8b82c9b2248e7c930682fd";
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests;

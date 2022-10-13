442f3fbfd6be231b57a7a2ac4fd0a15f819bc552
Lucas Cabral <lucascabral6008@gmail.com>
2022-10-13 16:01:54 -0300

    adding row component and cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import Main from '../components/Main'
+import React from 'react';
+import Main from '../components/Main';
+import Row from '../components/Row';
+import requests from "../Request"
 
 const Home = () => {
   return (
-    <Main />
+    <>
+        <Main />
+        <Row title="Up Coming" fetchUrl={requests.requestUpcoming} />
+        <Row title="Popular" fetchUrl={requests.requestPopular} />
+        <Row title="Top Rated" fetchUrl={requests.requestTopRated} />
+        <Row title="Trending" fetchUrl={requests.requestTrending} />
+        <Row title="Horror" fetchUrl={requests.requestHorror} />
+    </>
   )
 }
 
